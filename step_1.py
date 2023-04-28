faeea4976c2cce4db136e799769d3f7946e3fe6f
Aman <jiddewar.amandeep@gmail.com>
2023-04-27 20:38:51 -0400

    Create step_1.py

---
+++ step_1.py
@@ -0,0 +1,26 @@
+# This is the first step to get started with selenium
+
+# Check what is installed
+# $ pip freeze
+
+# Install Selenium
+# $ pip install selenium
+
+# Install html-testRunner
+# $ pip install html-testRunner
+
+# Check if installation was done, use one of below
+# $ pip freeze
+# $ pip list
+
+# See the summary of selenium & html-testRunner package
+# $ pip show selenium
+# $ pip show html-testRunner
+
+# Check for broken dependencies
+# $ pip check selenium
+# $ pip check html-testRunner
+
+# Check the documentations
+# https://pypi.org/project/html-testRunner/
+# https://pypi.org/project/selenium/
