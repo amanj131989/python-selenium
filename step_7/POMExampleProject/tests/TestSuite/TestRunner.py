0aa5ca6f06304e5cd5f3b94debf6bc31aee7a80e
Aman <jiddewar.amandeep@gmail.com>
2023-05-02 22:23:42 -0400

    Update TestRunner.py

--- step_7/POMExampleProject/tests/TestSuite/TestRunner.py
+++ step_7/POMExampleProject/tests/TestSuite/TestRunner.py
@@ -1,1 +1,20 @@
+from unittest import TestLoader, TestSuite, TextTestRunner
+from tests.Scripts.test_web_form import WebFormTestCase
+from tests.Scripts.test_submit_page import SubmitTestCase
+from HtmlTestRunner import HTMLTestRunner
+
+
+if __name__ == "__main__":
+    test_loader = TestLoader()
+    # Test Suite is used since there are multiple test cases
+    test_suite = TestSuite((
+        test_loader.loadTestsFromTestCase(WebFormTestCase),
+        test_loader.loadTestsFromTestCase(SubmitTestCase),
+    ))
+
+    test_runner = TextTestRunner(verbosity=2)
+    # test_runner = HTMLTestRunner(output='example_suite')
+    # runner.run(test_suite)
+    
+    test_runner.run(test_suite)
 
