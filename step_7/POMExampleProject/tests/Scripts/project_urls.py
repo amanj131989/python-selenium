db57c9039ea780389b4fc659d09654bb332ec2ab
Aman <jiddewar.amandeep@gmail.com>
2023-05-02 22:21:06 -0400

    Create project_urls.py

---
+++ step_7/POMExampleProject/tests/Scripts/project_urls.py
@@ -0,0 +1,2 @@
+web_form = "https://www.selenium.dev/selenium/web/web-form.html"
+submit_page = "https://www.selenium.dev/selenium/web/submitted-form.html?my-text=&my-password=&my-textarea=&my-readonly=Readonly+input&my-select=2&my-datalist=&my-file=&my-check=on&my-radio=on&my-colors=%23563d7c&my-date=05%2F17%2F2023&my-range=2&my-hidden="
